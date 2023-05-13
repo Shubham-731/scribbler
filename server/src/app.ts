5524031f143d71f73bd1d134a93a499f924267b4
Suraj Pandey <shpcore103@gmail.com>
2023-05-13 18:30:27 +0530

    Add response header

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,6 +17,7 @@ app.use((req, res, next) => {
         "Origin, X-Requested-With, Content-Type, Accept"
     )
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+    res.header("Access-Control-Expose-Headers", "Set-Cookie")
     next()
 })
 app.use(express.json())
